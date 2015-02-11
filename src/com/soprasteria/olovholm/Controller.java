73bcd163797c637b56c7031ea27c9110ae1a9be8
olovholm <ola.lovholm@steria.no>
2015-02-11 22:50:49 +0100

    Adds changes to flow class, and exit criterias. Empty read line if non-numbers are entered (used to make a infit run as the buffer could not be read and still had content)

--- src/com/soprasteria/olovholm/Controller.java
+++ src/com/soprasteria/olovholm/Controller.java
@@ -1,5 +1,4 @@
 package com.soprasteria.olovholm;
-import jdk.internal.util.xml.impl.Input;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -13,6 +12,7 @@ public class Controller {
     private Model model;
     private boolean exit = false;
     private Scanner keyboard;
+    private static int CRAZY_ROUND = 3;
 
     public Controller() {
         keyboard = new Scanner(System.in);
@@ -21,18 +21,39 @@ public class Controller {
         this.loop();
     }
 
+
+    //TODO skriv om teksthåndteringen her. Mer i view klassen
     public void loop() {
-        while(!exit){
+        while(!exit) {
             view.printCursor();
-            NumberEvaluation eval = model.evaluateNumber(this.getNumber());
-            System.out.println(eval);
+            NumberEvaluation eval = NumberEvaluation.NOT_VALID;
 
-
-
-            if(eval == NumberEvaluation.TOO_MANY_GUESSES || eval == NumberEvaluation.CORRECT_TOO_MANY_GUESSES) {
-                exit = true;
+            if (model.getNumberOfGuesses() != CRAZY_ROUND && eval == NumberEvaluation.NOT_VALID) {
+                eval = model.evaluateNumber(this.getNumber());
+                view.printEval(eval);
+            } else {
+                eval = model.shenanigans(this.getNumber());
+                view.printEval(eval);
             }
 
+
+             if (eval == NumberEvaluation.CORRECT_GUESS) {
+                view.printCorrectBeforeConfirmed();
+                 eval = model.confirmCorrectGuess(this.getNumber());
+            }
+
+
+            if (eval == NumberEvaluation.TOO_MANY_GUESSES || eval == NumberEvaluation.CORRECT_TOO_MANY_GUESSES || eval == NumberEvaluation.CORRECT_GUESS_CONFIRMED) {
+                exit = true;
+
+                 if (eval == NumberEvaluation.CORRECT_TOO_MANY_GUESSES) {
+                    view.printCloseButNoSigar();
+
+                } else if (eval == NumberEvaluation.CORRECT_GUESS_CONFIRMED) {
+                    view.printHurrayHurray();
+
+                }
+            }
         }
     }
 
@@ -42,14 +63,10 @@ public class Controller {
             int myint = keyboard.nextInt();
             return myint;
         } catch (InputMismatchException e){
+            keyboard.nextLine();
             view.printWrongInputMessage();
         }
         return -999;
     }
-
-
-
-
-
 
 }
