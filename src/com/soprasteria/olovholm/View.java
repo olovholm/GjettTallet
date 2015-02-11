276479d308511897e1a8d01b51304ddef73e2917
olovholm <ola.lovholm@steria.no>
2015-02-11 23:09:23 +0100

    Added new strings and more print mapping.

--- src/com/soprasteria/olovholm/View.java
+++ src/com/soprasteria/olovholm/View.java
@@ -1,18 +1,18 @@
 package com.soprasteria.olovholm;
 
 
-import javax.xml.transform.sax.SAXSource;
-
 /**
  * Created by olovholm on 02.02.2015.
  */
 public class View {
 
     public final static String CURSOR = "Gjett ett nummer: >";
+    public final static String TOO_MANY_TURNS = "Da har du forsøkt nok for denne gang. Start igjen for å få blanke ark og nye fargestifter";
     public final static String WRONG_INPUT_MESSAGE = "Dette programmet kan kun forstå tall. Forsøk med et heltall, snille deg";
     public final static String NUMBER_TOO_HIGH = "Tallet du har gjettet er for høyt, vennligst prøv igjen.";
     public final static String NUMBER_TOO_LOW =  "Tallet du har gjettet er for lavt, vennligst prøv igjen.";
-    public final static String NUMBER_CORRECT = "Gratulerer, du har gjettet riktig! Kan du gjenta det?";
+    public final static String NUMBER_CORRECT_CONFIRMED = "Du gjettet riktig! Well done";
+    public final static String NUMBER_CORRECT = "Gratulerer, du har gjettet riktig! Kan du gjenta det? >";
     public final static String NUMBER_CORRECT_TOO_MANY_TURNS = "Du fant riktig tall, men har brukt mer enn 7 forsøk. Prøv igjen og se om du kan forbedre deg.";
     public final static String NUMBER_GUESSED_BEFORE = "Du har gjettet dette tallet før, og vet dermed at det ikke er riktig. Du vil ikke miste et forsøk på dette.";
     public final static String INSTRUCTION_TEXT = "Velkommen til \"Gjett tallet\". Målet med leken er å gjette et tilfeldig valgt tall mellom 1 og 100 på 7 eller færre forsøk, lykketreff teller ikke, så du må kunne gjenta det for alle tall.\n" +
@@ -32,8 +32,8 @@ public class View {
     }
 
 
-    public void printResult(){
-
+    public void printResult(NumberEvaluation eval){
+        System.out.println(eval);
     }
 
     public void printCursor(){
@@ -42,6 +42,21 @@ public class View {
 
     public void printWrongInputMessage(){
         System.out.println(WRONG_INPUT_MESSAGE);
+    }
+
+    public void printEval(NumberEvaluation eval) {
+        if (eval == NumberEvaluation.TOO_HIGH) {
+            System.out.println(NUMBER_TOO_HIGH);
+        } else if (eval == NumberEvaluation.TOO_LOW){
+            System.out.println(NUMBER_TOO_LOW);
+        } else if (eval == NumberEvaluation.GUESSED_ALREADY){
+            System.out.println(NUMBER_GUESSED_BEFORE);
+        } else if (eval == NumberEvaluation.TOO_MANY_GUESSES){
+            System.out.println(TOO_MANY_TURNS);
+        }
+        else {
+            System.out.println(eval);
+        }
 
     }
 
@@ -49,7 +64,13 @@ public class View {
 
 
 
-
-
-
+    public void printCorrectBeforeConfirmed() {
+        System.out.print(NUMBER_CORRECT);
+    }
+    public void printCloseButNoSigar() {
+        System.out.println(NUMBER_CORRECT_TOO_MANY_TURNS);
+    }
+    public void printHurrayHurray() {
+        System.out.println(NUMBER_CORRECT_CONFIRMED);
+    }
 }
